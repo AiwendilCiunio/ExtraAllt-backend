f5ba4c7361eb780222e526ea2b9e0bcb5190d624
AiwendilCiunio <ciunio@gmail.com>
2025-11-05 20:29:07 +0100

    test surround checkout method with try catch to locate error

--- src/main/java/com/stocksimulator/stocksimulator/payment/PaymentService.java
+++ src/main/java/com/stocksimulator/stocksimulator/payment/PaymentService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
 
@@ -22,27 +23,33 @@ public class PaymentService {
 
     public String checkout(PaymentRequest request) throws Exception {
 
-        SessionCreateParams params = SessionCreateParams.builder()
-                .setMode(SessionCreateParams.Mode.PAYMENT)
-                .setSuccessUrl(successUrl)
-                .setCancelUrl(cancelUrl)
-                .addLineItem(
-                        SessionCreateParams.LineItem.builder()
-                                .setQuantity(1L)
-                                .setPriceData(
-                                        SessionCreateParams.LineItem.PriceData.builder()
-                                                .setCurrency("sek")
-                                                .setUnitAmount(request.getAmount() * 100)
-                                                .setProductData(
-                                                        SessionCreateParams.LineItem.PriceData.ProductData.builder()
-                                                                .setName(request.getDescription())
-                                                                .build())
-                                                .build())
-                                .build())
-                .build();
+        try {
+            SessionCreateParams params = SessionCreateParams.builder()
+                    .setMode(SessionCreateParams.Mode.PAYMENT)
+                    .setSuccessUrl(successUrl)
+                    .setCancelUrl(cancelUrl)
+                    .addLineItem(
+                            SessionCreateParams.LineItem.builder()
+                                    .setQuantity(1L)
+                                    .setPriceData(
+                                            SessionCreateParams.LineItem.PriceData.builder()
+                                                    .setCurrency("sek")
+                                                    .setUnitAmount((long) Math.round(request.getAmount() * 100))
+                                                    .setProductData(
+                                                            SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                                                    .setName(request.getDescription())
+                                                                    .build())
+                                                    .build())
+                                    .build())
+                    .build();
 
-        Session session = Session.create(params);
-        return session.getId();
+            Session session = Session.create(params);
+            return session.getId();
+        } catch (StripeException e) {
+
+            e.printStackTrace();
+            throw new RuntimeException("Stripe checkout failed" + e.getMessage());
+        }
     }
 
     public void processWebhook(String payload, String signature) {
